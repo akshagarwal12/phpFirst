c9bc01c63832d97e3c3d2ac3f7d4e7e3c3e9000b
Aksh Agarwal <aksh.agarwal0512@gmail.com>
2020-10-31 14:11:31 +0530

    First Release

---
+++ jumbo.php
@@ -0,0 +1,7 @@
+<div class="jumbotron">
+  <h1 class="display-4">TheRandom!</h1>
+  <p class="lead">He Makes Memes</p>
+  <hr class="my-4">
+  <p>He really do make memes doe</p>
+  <a class="btn btn-primary btn-lg" href="info.php" role="button">Learn more</a>
+</div>
